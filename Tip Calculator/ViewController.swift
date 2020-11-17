6154d8d6cc96908025eed65b1393cefe7bdcacbf
Brocktho <71537341+Brocktho@users.noreply.github.com>
2020-11-16 23:45:16 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Brock Donahue on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
